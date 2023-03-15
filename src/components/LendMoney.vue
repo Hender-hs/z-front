4ab5228fcfe2e673a54e28f945c12c8510aa6f5e
hender-hs <henderson.f.p@hotmail.com>
2023-03-15 12:56:21 -0300

    feat: add new inputs to a complete user experience

--- src/components/LendMoney.vue
+++ src/components/LendMoney.vue
@@ -1,13 +1,64 @@
 <script setup lang="ts">
   import LendResults from "./LendResults.vue";
-  import { reactive } from "vue";
+  import { reactive, onMounted } from "vue";
   import axios from "axios";
+  import { API_URL } from "../main";
+
 
   const moneyAmount = reactive({ 
 	value: "", 
 	setValue(val: string) {
 	  this.value = val;
 	}
+  });
+
+  const institutions = reactive({ 
+	value: [] as any[], 
+	setValue(val: any[]) {
+	  this.value = val.map(el => el.chave);
+	}
+  });
+
+  const insurances = reactive({ 
+	value: [] as any[], 
+	setValue(val: any[]) {
+	console.log(val);
+	  this.value = val.map((el: any) => el.chave);
+	}
+  });
+
+  const institutionSelected = reactive({ 
+	value: "", 
+	setValue(val: string) {
+	  this.value = val;
+	}
+  });
+
+  const insuranceSelected = reactive({ 
+	value: "", 
+	setValue(val: string) {
+	console.log(val);
+	  this.value = val;
+	}
+  });
+
+  const quoteSelected = reactive({ 
+	value: "", 
+	setValue(val: string) {
+	  this.value = val;
+	}
+  });
+
+  onMounted(() => {
+	Promise.all([
+	  axios.get(`${API_URL}/instituicao`), 
+	  axios.get(`${API_URL}/convenio`), 
+	]).then((res: any) => {
+	  const resInstitution = res[0];
+	  const resInsurances = res[1];
+	  institutions.setValue(resInstitution.data);
+	  insurances.setValue(resInsurances.data);
+	});
   });
 
   const response = reactive({
@@ -25,6 +76,10 @@
 
 	  // @ts-ignore
 	  this.value = serializedData.flat();
+	},
+	wasRequested: false,
+	setWasRequested(val: boolean) {
+	  this.wasRequested = val;
 	}
   });
 
@@ -41,6 +96,10 @@
 	}
 	return newArr;
   }
+
+  const selectHandler = (setValue: (v: any) => void) => (evnt: any) => {
+	setValue(evnt.target.value);	
+  };
   
   const inputChangeHandler = (evnt: any) => {
 	const { value } = evnt.target;
@@ -52,17 +111,20 @@
   
   const requestHandler = async () => {
 	const amountOfMoneyRequested = Number(moneyAmount.value.replace(/\D/g, ""));
+	const institSlcted = institutionSelected.value;
+	const insurSlcted = insuranceSelected.value;
 	const payload = {
 	  valor_emprestimo: amountOfMoneyRequested,
-	  instituicoes: [],
-	  convenios: [],
-	  parcela: 0
+	  instituicoes: institSlcted ? [institSlcted] : [],
+	  convenios: insurSlcted ? [insurSlcted] : [] ,
+	  parcela: quoteSelected.value || 0
 	};
 	const url = `http://127.0.0.1:8000/api/simular`;
 
 	const res = await axios.post(url, payload);
 
 	response.setValue(res.data);
+	response.setWasRequested(true);
   }
 </script>
 
@@ -73,14 +135,37 @@
 	  <p>R$</p> 
 	  <input @keyup="inputChangeHandler" v-model="moneyAmount.value" class="input" placeholder=""/> 
 	</div>
+	<div class="input-section"> 
+	  <select @change="selectHandler(institutionSelected.setValue)" v-model="institutionSelected.value" class="input">
+		<option value="">selectione a instituição</option> 
+		<option v-for="institution in institutions.value" :value="institution">{{ institution }}</option> 
+	  </select>
+	</div>
+	<div class="input-section"> 
+	  <select @change="selectHandler(insuranceSelected.setValue)" v-model="insuranceSelected.value" class="input">
+		<option value="">Selecione o concênio</option> 
+		<option v-for="insurance in insurances.value" :value="insurance">{{ insurance }}</option> 
+	  </select>
+	</div>
+	<div class="input-section"> 
+	  <select @change="selectHandler(quoteSelected.setValue)" v-model="quoteSelected.value" class="input">
+		<option value="">Selecione o número de parcelas</option> 
+		<option value="36">36</option> 
+		<option value="48">48</option> 
+		<option value="60">60</option> 
+		<option value="72">72</option> 
+		<option value="84">84</option> 
+	  </select>
+	</div>
 	<button :disabled="!moneyAmount.value" @click="requestHandler" class="button" >Solicitar</button>
   </div>
-  <div v-if="response.value.length">
-	<h4>Resultados</h4>
-	<div class="boxes-body">
-	  <LendResults v-for="item in response.value" :valor_solicitado="moneyAmount.value" v-bind="item" />
+	<div v-if="response.value.length">
+	  <h4>Propostas</h4>
+	  <div class="boxes-body">
+		<LendResults v-for="item in response.value" :valor_solicitado="moneyAmount.value" v-bind="item" />
+	  </div>
 	</div>
-  </div>
+	<h3 v-else-if="response.wasRequested" class="warning-message">Hoje não temos propostas interessantes para você neste momento :(</h3>
 </template>
 
 <style>
@@ -123,5 +208,8 @@
 	border-radius: 5px;
 	height: 350px;
 	overflow-y: scroll;
+  }
+  .warning-message {
+	color: #08d280;
   }
 </style>
